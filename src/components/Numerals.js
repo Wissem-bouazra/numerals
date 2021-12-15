fd5be90e368174ceb08073a13d082b723a1a0e1c
wissembouazra <wissembouazra@gmail.com>
2021-12-15 14:06:54 -0800

    finale version

--- src/components/Numerals.js
+++ src/components/Numerals.js
@@ -1,18 +1,13 @@
 import React, {useState} from "react";
 import inWords from './Convert'
-import Convert from "./Convert";
 
-function Numerals(props) {
+function Numerals() {
     const [input, setInput] = useState('');
     const handleChange = e => {
         setInput(e.target.value);
     };
     const handleSubmit = e => {
         e.preventDefault();
-        props.onSubmit({
-            id: Math.floor(Math.random() * 10000),
-            text: input
-        });
     }
 
     return (
@@ -23,11 +18,11 @@ function Numerals(props) {
                        value={input} name='text'
                        className='numeral-input'
                        onChange={handleChange}
-                       autoComplete="off"
+                       autoComplete="off" // change value to on for suggestions
                 />
             </div>
             <div>
-                <textarea disabled type='' value={inWords(input)} className='convert'/>
+                <textarea disabled  value={inWords(input)} className='convert'/>
             </div>
         </form>
 
